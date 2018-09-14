b41955d6673666cb86d1fc20c015d90093d4d7c4
16-2505-072-6 <w072ff@gmail.com>
2018-09-14 16:25:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/enpit_p20/a914ensyuu/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.enpit_p20.a914ensyuu
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
